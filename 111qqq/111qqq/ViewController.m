6beeebbe86584b41cb82ad16f64b6176d5baec2d
shang <shang@qq.com>
2016-01-23 08:48:58 +0800

    demo4

---
+++ 111qqq/111qqq/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  111qqq
+//
+//  Created by CC on 16/1/23.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    NSLog(@"aaaaaddddd");
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
